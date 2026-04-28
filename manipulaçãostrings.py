acbf1ecf7de6d336e3a58259591497eda5774984
João Vitor de Medeiros Castro Ferreira <jvmedeiros7@hotmail.com>
2026-04-28 14:48:40 -0300

    Create manipulaçãostrings.py

---
+++ manipulaçãostrings.py
@@ -0,0 +1,101 @@
+#Metodos úteis da Classe String
+
+''' curso = "PyThOn"
+
+print(curso.upper())
+#Python
+
+print(curso.lower())
+#python
+
+print (curso.title())
+#Python
+
+curso1 = " Python "
+
+print(curso1.strip())
+#Python
+
+print(curso1.lstrip())
+#"Python   "
+
+print(curso1.rstrip())
+#"   Python"
+
+
+curso2 = "Python"
+
+print(curso2.center(10, "#"))
+#"##Python##"
+
+print(".".join(curso2))
+#"P.y.th.h.o.n" ''' 
+
+
+#f-string
+
+''' nome = input("Qual seu nome?")
+
+print(f"O seu nome é : {nome.upper()} ")
+
+
+alt = float(input("Qual sua altura??"))
+
+print(f"sua altura é {alt:10.2f}")
+
+
+pi = float(input("Qual valor de PI?"))
+
+print(f"O valor de pi é: {pi:.2f}") ''' 
+
+
+
+#Fatiamento de Strings
+
+''' nome = "João Vitor de Medeiros"
+
+print(nome[0]) # Passa o que estiver no indice
+#"J"
+
+print(nome[:4]) # Passa tudo que vinher da esquerda até 10 caracteres
+#"João"
+
+print(nome[13:]) # Passa tudo que vinher da direita até 10 caracteres
+#" de Medeiros" 
+
+print(nome[5:12])
+#" Vitor de "
+
+print(nome[10:16:2])
+#" eM"
+
+print(nome[:])
+#"João Vitor de Medeiros"
+
+print(nome[::-1])
+#"soriedeM ed rotiV oãoJ" ''' 
+
+#String Multiplas Linhas
+
+nome = "João Vitor de Medeiros"
+
+mensagem = f""" 
+    Olá meu nome é {nome}
+Eu estou aprendendo Python.
+        Essa mensagem tem diferentes recuos. 
+"""
+
+print(mensagem)
+
+
+print("""
+    ===MENU===
+      
+      1 - PRODUTO 1
+      2 - PRODUTO 2
+      3 - PRODUTO 3
+      0 - SAIR
+
+    =========== """
+      )
+
