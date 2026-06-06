f35fff10dcba76f332a9948fab4138d307328114
João Vitor de Medeiros Castro Ferreira <jvmedeiros7@hotmail.com>
2026-06-06 11:58:38 -0300

    estudos

--- cursoemvideoPython.py/exerciciolistas.py
+++ cursoemvideoPython.py/exerciciolistas.py
@@ -28,7 +28,7 @@ print(f'A soma dos valores digitados é {soma}')'''
 
 #Desafio 080
 
-numeros = list()
+'''numeros = list()
 
 for c in range(0,5):
     n = int(input('Digite um valor: '))
@@ -44,4 +44,72 @@ for c in range(0,5):
                 break
             pos += 1
 
-print(f'Os valores digitados em ordem são: {numeros}')
+print(f'Os valores digitados em ordem são: {numeros}')'''
+
+#Desafio 081
+
+'''valores = []
+
+while True:
+    n = int(input('Digite um valor: '))
+    if n not in valores:
+        valores.append(n)
+        print('Valor adicionado com sucesso...')
+    else:
+        print('Valor duplicado! Não vou adicionar...')
+    resposta = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+    if resposta in 'N':
+        break
+
+print(f"Você digitou a quantidade de valores : {len(valores)}")
+
+valores.sort(reverse=True)
+print(f'Os valores em ordem decrescente são: {valores}')
+
+x = input('Digite um valor para saber se ele faz parte da lista: ')
+
+if x in valores:
+    print(f'O valor {x} faz parte da lista!')
+else:
+    print(f'O valor {x} não foi encontrado na lista!')
+
+print('Fim do programa!')'''
+
+#Desafio 082
+
+'''valores_pares = []
+valores_impares = []
+valores_totais = []
+
+while True:
+    n = int(input('Digite um valor: '))
+    valores_totais.append(n)
+    if n % 2 == 0:
+        valores_pares.append(n)
+    else:
+        valores_impares.append(n)
+    resposta = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+    if resposta in 'N':
+        break
+print(f'Os valores pares digitados foram: {valores_pares}')
+print(f'Os valores ímpares digitados foram: {valores_impares}')
+print(f'Os valores totais digitados foram: {sorted(valores_totais)}')'''
+
+#Desafio 083
+
+expr = str(input('Digite uma expressão: ')).strip()
+pilha = []
+
+for simb in expr:
+    if simb == '(':
+        pilha.append('(')
+    elif simb == ')':
+        if len(pilha) > 0:
+            pilha.pop()
+        else:
+            pilha.append(')')
+            break
+if len(pilha) == 0:
+    print('Sua expressão é válida!')
+else:
+    print('Sua expressão é inválida!')
